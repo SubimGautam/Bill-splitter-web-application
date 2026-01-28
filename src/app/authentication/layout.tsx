e1efeb330aaf245021727051e692fd6af8f46264
Subim Gautam <subimgautam@gmail.com>
2026-01-28 13:16:57 +0545

    Updated

--- src/app/authentication/layout.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50">
-      {children}
-    </div>
-  );
-}
